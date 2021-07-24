c3900cfe34e3fa2a733dfb90e0bb543a92855cfb
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-24 18:52:01 +0530

    [soubarnika]Implementaion of collections in c#

---
+++ Non-GenericCollection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Non_GenericCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
